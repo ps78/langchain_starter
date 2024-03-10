ba49810e09762c09cb056a7a80a556141e9dd636
Pascal Simon <mail@pascalsimon.com>
2024-03-10 14:13:22 +0100

    simplified code

--- main_petname.py
+++ main_petname.py
@@ -1,12 +1,8 @@
-#
-# Generates pet names, based od: https://www.youtube.com/watch?v=lG7Uxts9SXs
-#
 import os
 from dotenv import load_dotenv
 from langchain_openai import AzureChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
-from langchain.agents import load_tools, initialize_agent, AgentType
 from utils import load_certs
 
 load_certs()
@@ -15,7 +11,7 @@ load_dotenv()
 def generate_pet_name(animal_type :str, color :str, n :int) -> str:
     prompt_template_name = PromptTemplate(
         input_variables=["animal_type", "color", "n"],
-        template="I have a {color} {animal_type} pet and I want a cool name for it. Suggest me {n} cool names for my pet"
+        template="I have a {color} {animal_type} pet and I want a cool name for it. Suggest me {n} names for my pet"
     )
     llm = AzureChatOpenAI(temperature=0.8, azure_deployment=os.getenv("AZURE_GPT3_DEPLOYMENT"))
     name_chain = LLMChain(llm=llm, prompt=prompt_template_name, output_key="pet_name")
@@ -26,20 +22,4 @@ def generate_pet_name(animal_type :str, color :str, n :int) -> str:
     })
     return response["pet_name"]
 
-def langchain_agent():
-    llm = AzureChatOpenAI(temperature=0.5, azure_deployment=os.getenv("AZURE_GPT3_DEPLOYMENT"))
-    tools = load_tools(tool_names=["wikipedia", "llm-math"], llm=llm)
-    agent = initialize_agent(
-        tools=tools,
-        llm=llm,
-        agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-        verbose=True
-    )
-    result = agent.run("What is the average age of a dog? Multiply the age by 3")
-    print(result)
-
-#
-# Main
-#
-print(generate_pet_name("dog", "pink", 5))
-#langchain_agent()
+print(generate_pet_name("elephant", "purple", 5))
